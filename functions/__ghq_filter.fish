5c6306c9b5400057d74bb346453b9d7da2bd9766
Decors <decors1215@gmail.com>
2016-04-22 16:05:05 +0900

    Update

--- functions/__ghq_filter.fish
+++ functions/__ghq_filter.fish
@@ -1,9 +1,10 @@
 function __ghq_filter -d 'Repository filtering'
     set -q GHQ_FILTER; or set -l GHQ_FILTER peco
+    set -l query (commandline -b)
     switch "$GHQ_FILTER"
         case 'peco'
-            __ghq_filter_peco
+            __ghq_filter_peco $query
         case 'fzf'
-            __ghq_filter_fzf
+            __ghq_filter_fzf $query
     end
 end
--- functions/__ghq_filter_peco.fish
+++ functions/__ghq_filter_peco.fish
@@ -1,14 +1,8 @@
 function __ghq_filter_peco -d 'Repository filtering with peco'
-    if type -q peco
-        set -q argv; and set flags --query "$argv"; or set flags
-        ghq list --full-path | peco $flags | read -l select
-        if [ $select ]
-            builtin cd "$select"
-            commandline -f repaint
-        else
-            commandline ''
-        end
-    else
-        echo "Please install 'peco'"
+    set -q argv; and set flags --query "$argv"; or set flags
+    ghq list --full-path | peco $flags | read -l select
+    if [ $select ]
+        builtin cd "$select"
     end
+    commandline -f repaint
 end
