66c5336082efe959029165dae8843bb7f00549e0
Decors <decors1215@gmail.com>
2019-03-13 21:22:52 +0900

    Remove __fzfcmd compatibility

--- functions/__ghq_crtl_g.fish
+++ functions/__ghq_crtl_g.fish
@@ -1,13 +1,10 @@
 function __ghq_crtl_g -d 'Repository search'
     set -l selector
     [ -z "$GHQ_SELECTOR" ]; and set selector fzf; or set selector $GHQ_SELECTOR
-    if [ "$selector" = fzf ]
-        functions -q __fzfcmd; and set selector (__fzfcmd)
-    end
     set -l query (commandline -b)
     [ -n "$query" ]; and set flags --query="$query"; or set flags
     switch "$selector"
-        case fzf __fzfcmd peco percol fzy
+        case fzf peco percol fzy
             ghq list --full-path | eval "$selector" $flags | read select
         case \*
             printf "\nERROR: plugin-ghq is not support '$selector'.\n"
