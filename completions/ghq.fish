69f5d7d1864412215498c9a8ca4a5bb8049d8e18
Decors <decors1215@gmail.com>
2019-05-08 19:17:29 +0900

    Update completions

--- completions/ghq.fish
+++ completions/ghq.fish
@@ -23,16 +23,29 @@ function __fish_ghq_help_topics
     end
 end
 
+function __fish_ghq_vcs
+    for c in git subversion git-svn mercurial darcs
+        printf "%s\tVCS\n" $c
+    end
+    printf "github\tAlias for git\n"
+    printf "svn\tAlias for subversion\n"
+    printf "hg\tAlias for mercurial\n"
+end
+
 complete -f -c ghq -n "__fish_ghq_needs_command" -a help -d "Shows a list of commands or help for one command"
 complete -f -c ghq -n "__fish_ghq_using_command help" -a "(__fish_ghq_help_topics)"
 
 complete -f -c ghq -n "__fish_ghq_needs_command" -a get -d "Clone/sync with a remote repository"
 complete -f -c ghq -n "__fish_ghq_using_command get" -l update -s u -d "Update local repository if cloned already"
-complete -f -c ghq -n "__fish_ghq_using_command get" -l shallow -d "Do a shallow clone"
 complete -f -c ghq -n "__fish_ghq_using_command get" -s p -d "Clone with SSH"
+complete -f -c ghq -n "__fish_ghq_using_command get" -l shallow -d "Do a shallow clone"
+complete -f -c ghq -n "__fish_ghq_using_command get" -l look -s l -d "Look after get"
+complete -f -c ghq -n "__fish_ghq_using_command get" -l vcs -d "Specify VCS backend for cloning" -r -a "(__fish_ghq_vcs)"
+complete -f -c ghq -n "__fish_ghq_using_command get" -l silent -s s -d "Clone or update silently"
 
 complete -f -c ghq -n "__fish_ghq_needs_command" -a list -d "List local repositories"
 complete -f -c ghq -n "__fish_ghq_using_command list" -l exact -s e -d "Perform an exact match"
+complete -f -c ghq -n "__fish_ghq_using_command list" -l vcs -d "Specify VCS backend for matching" -r -a "(__fish_ghq_vcs)"
 complete -f -c ghq -n "__fish_ghq_using_command list" -l full-path -s p -d "Print full paths"
 complete -f -c ghq -n "__fish_ghq_using_command list" -l unique -d "Print unique subpaths"
 
@@ -41,8 +54,12 @@ complete -f -c ghq -n "__fish_ghq_using_command look" -a "(ghq list)" -d "local 
 
 complete -f -c ghq -n "__fish_ghq_needs_command" -a import -d "Bulk get repositories from stdin"
 complete -f -c ghq -n "__fish_ghq_using_command import" -l update -s u -d "Update local repository if cloned already"
-complete -f -c ghq -n "__fish_ghq_using_command import" -l shallow -d "Do a shallow clone"
 complete -f -c ghq -n "__fish_ghq_using_command import" -s p -d "Clone with SSH"
+complete -f -c ghq -n "__fish_ghq_using_command import" -l shallow -d "Do a shallow clone"
+complete -f -c ghq -n "__fish_ghq_using_command import" -l look -s l -d "Look after get"
+complete -f -c ghq -n "__fish_ghq_using_command import" -l vcs -d "Specify VCS backend for cloning" -r -a "(__fish_ghq_vcs)"
+complete -f -c ghq -n "__fish_ghq_using_command import" -l silent -s s -d "Clone or update silently"
+complete -f -c ghq -n "__fish_ghq_using_command import" -l parallel, -s P -d "[Experimental] Import parallely"
 
 complete -f -c ghq -n "__fish_ghq_needs_command" -a root -d "Show repositories' root"
 complete -f -c ghq -n "__fish_ghq_using_command root" -l all -d "Show all roots"
